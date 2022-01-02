30ae971aa3a78d7394fc674da822d3862d38acfb
Naimur Rahman <naeemhasan28@gmail.com>
2022-01-02 20:29:08 +0600

    added options and documentations

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -2,13 +2,37 @@ import axios from 'axios'
 import React from 'react'
 import { useQuery } from 'react-query'
 
+/*
+Let's talk about the options that goes as 3rd param of useQuery.
+
+## cacheTime --> It caches the data for certain times, so that it doesn't refetch the data when loading. The default value is 5 minutes.
+
+## staleTime --> It doesn't refetch the data for certain times (If cacheTime is activated, it will refetch in the background though, but if staleTime is activated, it won't refetch for certain time), so that it doesn't refetch the data when loading. The default value is 0 second.
+
+## refetchOnMount --> It refetches the data when the component is mounted. The default value is true. The options are: false, true, 'always'.
+if true -> It will refresh the data when the component is mounted and only refresh when staleTime is finished.
+if false -> It won't refresh the data when the component is mounted.
+if 'always' -> It will refresh the data when the component is mounted and doesn't look at staleTime is activated or not.
+
+## refetchOnWindowFocus --> It refetches the data when the window is focused. The default value is true, it enables the power to refetch data if tha data is changed in the backend.
+The options for refetchOnWindowFocus is the same as refetchOnMount.
+
+## refetchInterval --> It refetches the data every certain time. The default value is 0 second. It stops works when it looses its focus. It continue to refetch when refetchIntervalInBackground is set to true.
+
+## refetchIntervalInBackground --> It enables refetchInverval to refetch even in the background. Options are: true, false. The default value is false.
+ */
+
 const RQSuperHeroes = () => {
   const { isLoading, data, isError, error } = useQuery(
     'super-heroes',
     () => axios.get('http://localhost:4000/superheroes'),
     {
-      cacheTime: 10 * 1000 * 60, // In this time duration, it cache tha data and doesn't refetch when loading (Default: 5 mins)
-      staleTime: 30 * 1000 // In this time duration, it won't refetch the data (Default: 0)
+      cacheTime: 10 * 1000 * 60,
+      staleTime: 30 * 1000,
+      refetchOnMount: true,
+      refetchOnWindowFocus: true,
+      refetchInterval: 2 * 1000,
+      refetchIntervalInBackground: true
     }
   )
 
